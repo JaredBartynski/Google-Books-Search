6dab5068133c4a61815afc55ca45900980425970
JaredBartynski <70081376+JaredBartynski@users.noreply.github.com>
2020-11-13 02:01:07 -0500

    adding heroku support

---
+++ client/build/precache-manifest.691bd27a0c1a18f62b317803c0a7e70f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9a281dbfe7c34430e00c3aa73961c38",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cb43d5791c03399e9ec2",
+    "url": "./static/css/main.36e36df1.chunk.css"
+  },
+  {
+    "revision": "e71910ff139c7a44a1a4",
+    "url": "./static/js/2.add64ed1.chunk.js"
+  },
+  {
+    "revision": "f323b4f96b03ab20856e7bf51591040f",
+    "url": "./static/js/2.add64ed1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb43d5791c03399e9ec2",
+    "url": "./static/js/main.70a83440.chunk.js"
+  },
+  {
+    "revision": "6f9f03a2ff23e012d744",
+    "url": "./static/js/runtime-main.519075df.js"
+  }
+]);
